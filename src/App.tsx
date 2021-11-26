5e22ec5f5e54cf58415101ab0cf80fefe2929698
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2021-11-26 16:36:32 +0900

    io로 무한 스크롤링 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useGetComicByPageQuery } from './services/comic';
+import Loader from './components/Loader.component';
 
 export default function App() {
   const [page, setPage] = useState(1);
   const [comics, setComics] = useState([]);
   const { data, error, isLoading } = useGetComicByPageQuery(page);
 
+  // io
+  const [target, setTarget] = useState(null);
+
+  const onIntersect = async ([entry], observer) => {
+    if (entry.isIntersecting && !isLoading) {
+      observer.unobserve(entry.target);
+      setTimeout(() => {
+        if (page < 5) setPage(page + 1);
+      }, 1000);
+      observer.observe(entry.target);
+    }
+  };
+
   useEffect(() => {
     if (data) setComics(comics.concat(data.data));
   }, [data]);
 
-  if (isLoading) return <div>Loading...</div>;
-  if (!comics) return <div>Missing comic!</div>;
+  useEffect(() => {
+    let observer;
+    if (target && !isLoading) {
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 1,
+      });
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+  }, [target, isLoading, page]);
+
+  // if (isLoading) return <div>Loading...</div>;
+  // if (!comics) return <div>Missing comic!</div>;
 
   // Using a query hook automatically fetches data and returns query values
   // Individual hooks are also accessible under the generated endpoints:
@@ -22,11 +47,14 @@ export default function App() {
     <div className="App">
       <>
         {comics.map((comic, idx) => (
-          <div>
+          <div key={comic.id}>
             {idx} {comic.title}
           </div>
         ))}
-        <button onClick={() => setPage(page + 1)}>page + 1</button>
+        <button onClick={() => setPage(page + 1)}>page add</button>
+        <div ref={setTarget} className="Target-Element">
+          {isLoading && <Loader />}
+        </div>
       </>
     </div>
   );
